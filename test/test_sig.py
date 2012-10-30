724de18fec76a5f526fa19dd5771bab4321dd05b
Mike Starr <go2starr@gmail.com>
2012-10-30 18:07:03 -0500

    Move testing utilities into own file

--- test/test_sig.py
+++ test/test_sig.py
@@ -1,30 +1,10 @@
 import itertools
-import operator
-import random
 
 from math import sqrt
 
+from utils import *
 from ..lsh import MinHashSignature, jaccard_sim
 
-################################################################################
-#  Testing utilities
-################################################################################
-def randset():
-    """Return a random set.  These values of n and k have wide-ranging
-    similarities between pairs.
-    """
-    n = random.choice(range(5, 20))
-    k = 10
-    return set( random.choice(range(k)) for _ in range(n) )
-
-def sigsim(X, Y, dim):
-    """Return the similarity of the two signatures"""
-    return sum(map(operator.eq, X, Y)) / float(dim)
-
-
-################################################################################
-# Le tests
-################################################################################
 def test_signature_length():
     """Signatures should have correct dimension"""
     dim = 100
